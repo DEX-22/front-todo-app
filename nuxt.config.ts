da0de73286f0f7c29a71a071bf69b40d3ef71a15
dexx <enriquesaenz0@gmail.com>
2024-11-29 11:59:41 -0500

    fix access login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   ssr: false,
-
   runtimeConfig: { 
     public:{apiUrl:process.env.NUXT_API_URL}
   },
